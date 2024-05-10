0c50fecfc54e7a9b04e0a79a8f28e44d5173a3ae
tonipepperoni0 <tonezbanan@gmail.com>
2024-05-10 03:38:38 +0200

    Added Cache black magic fuckery in the service worker.

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
+const staticCacheName = "site-static-v1";
+const assets = [
+    'index.html',
+    'app.js',
+    'ui.js',
+    'style.css'];
+
 self.addEventListener('install', evt => {
-    console.log("service worker has been installed");
+    //console.log("service worker has been installed");
+    evt.waitUntil(caches.open(staticCacheName).then(cache => {
+        console.log('caching shell assets');
+        cache.addAll(assets);
+    }))
 });
 
 self.addEventListener('activate', evt => {
-    console.log("service worker has been activated");
+    //console.log("service worker has been activated");
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            //console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    );
 });
 
 self.addEventListener('fetch', evt => {
-    console.log("fetch event", evt);
+    //console.log("fetch event", evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
 });
